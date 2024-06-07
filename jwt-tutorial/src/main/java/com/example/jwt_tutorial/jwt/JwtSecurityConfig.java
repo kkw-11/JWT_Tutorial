3d4babed4c5f2035f2747e3d3d9fb7fe8ef813f2
kiwon <kkw-11@naver.com>
2024-06-07 14:34:54 +0900

    feat:Adding JwtSecurityConfig Class

---
+++ jwt-tutorial/src/main/java/com/example/jwt_tutorial/jwt/JwtSecurityConfig.java
@@ -0,0 +1,24 @@
+package com.example.jwt_tutorial.jwt;
+
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+/**
+ * TokenProvider와 JwtFiler를 SecurityConfig에 적용할 때 사용할 JwtSecurityConig
+ */
+public class JwtSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+    private final TokenProvider tokenProvider;
+    public JwtSecurityConfig(TokenProvider tokenProvider) {
+        this.tokenProvider = tokenProvider;
+    }
+
+    @Override
+    public void configure(HttpSecurity http) {
+        http.addFilterBefore(
+                new JwtFilter(tokenProvider),
+                UsernamePasswordAuthenticationFilter.class
+        );
+    }
+}
